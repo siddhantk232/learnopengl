fe7dc3e1f17aa4e1865e15a0d7fdb00663f3dc38
siddhantCodes <siddhantk232@gmail.com>
2025-11-21 19:16:03 +0530

    exercises from the first chapter
    covers exercises from https://learnopengl.com/Getting-started/Hello-Triangle

--- src/main.cc
+++ src/main.cc
@@ -25,11 +25,18 @@ char const* const VERTEX_SHADER_SOURCE =
     "    gl_Position = vec4(aPos, 1.0);\n"
     "}\n";
 
-char const* const FRAGMENT_SHADER_SOURCE =
+char const* const FRAGMENT_SHADER_SOURCE_PURPLE =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main() {\n"
     "    FragColor = vec4(0.5, 0.0, 0.5, 1.0);\n"
+    "}\n";
+
+char const* const FRAGMENT_SHADER_SOURCE_YELLOW =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "    FragColor = vec4(0.82, 0.92, 0.20, 1.0);\n"
     "}\n";
 
 int checkCompileShader(char const* const* source, GLenum shaderType) {
@@ -82,12 +89,15 @@ int main() {
     GLuint vertexShader =
         checkCompileShader(&VERTEX_SHADER_SOURCE, GL_VERTEX_SHADER);
 
-    GLuint fragmentShader =
-        checkCompileShader(&FRAGMENT_SHADER_SOURCE, GL_FRAGMENT_SHADER);
+    GLuint fragmentShaderPurple =
+        checkCompileShader(&FRAGMENT_SHADER_SOURCE_PURPLE, GL_FRAGMENT_SHADER);
+
+    GLuint fragmentShaderYellow =
+        checkCompileShader(&FRAGMENT_SHADER_SOURCE_YELLOW, GL_FRAGMENT_SHADER);
 
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, fragmentShaderPurple);
     glLinkProgram(shaderProgram);
 
     {
@@ -100,44 +110,75 @@ int main() {
         }
     }
 
+    GLuint shaderProgram2 = glCreateProgram();
+    glAttachShader(shaderProgram2, vertexShader);
+    glAttachShader(shaderProgram2, fragmentShaderYellow);
+    glLinkProgram(shaderProgram2);
+
+    {
+        int success;
+        glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+        if (!success) {
+            char buf[512];
+            glGetProgramInfoLog(shaderProgram2, sizeof(buf), NULL, buf);
+            ERROR("shader program link: %s", buf);
+        }
+    }
+
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShaderPurple);
+    glDeleteShader(fragmentShaderYellow);
 
     // opengl normalised coords
     // clang-format off
-    float rectangle[] = {
-        0.5f  , 0.5f  , 0.0f , // top-right
-        0.5f  , -0.5f , 0.0f , // bottom-right
-        -0.5f , 0.5f  , 0.0f , // top-left
-        -0.5f , -0.5f , 0.0f , // bottom-left
+    float tri1[] = {
+        0.0f, 0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
     };
 
-    GLuint indices[] = {
-        0, 1, 3,
-        0, 2, 3,
+    float tri2[] = {
+        -0.5f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
     };
+
+    // GLuint indices[] = {
+    //     0, 1, 3,
+    //     0, 2, 3,
+    // };
     // clang-format on
 
-    GLuint VAO;  // vertex array object
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    GLuint VAO1;  // vertex array object
+    glGenVertexArrays(1, &VAO1);
+    glBindVertexArray(VAO1);
 
-    GLuint VBO;  // vertex buffer object
-    glGenBuffers(1, &VBO);
-
-    GLuint EBO;  // element buffer object
-    glGenBuffers(1, &EBO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                 GL_STATIC_DRAW);
+    GLuint VBO1;  // vertex buffer object
+    glGenBuffers(1, &VBO1);
 
     // 0. copy triangle in gpu memory
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tri1), tri1, GL_STATIC_DRAW);
 
     // 1. setup inputs (attributes) for vertex shader
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(rectangle[0]), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(tri1[0]),
+                          (void*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint VAO2;  // vertex array object
+    glGenVertexArrays(1, &VAO2);
+    glBindVertexArray(VAO2);
+
+    GLuint VBO2;  // vertex buffer object
+    glGenBuffers(1, &VBO2);
+
+    // 0. copy triangle in gpu memory
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tri2), tri2, GL_STATIC_DRAW);
+
+    // 1. setup inputs (attributes) for vertex shader
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(tri2[0]),
+                          (void*)0);
     glEnableVertexAttribArray(0);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);  // GL_LINE to draw wireframe
@@ -148,8 +189,12 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, NULL);
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glUseProgram(shaderProgram2);
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
